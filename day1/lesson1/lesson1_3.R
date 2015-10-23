b8e56108107a1036673bc41d8f529ae89a30b0af
Stefan Widgren <stefan.widgren@sva.se>
2015-10-23 10:36:45 +0200

    Add R-script and do-file to lesson 1 html file

--- day1/lesson1/lesson1_3.R
+++ day1/lesson1/lesson1_3.R
@@ -6,40 +6,31 @@
 ## you give them
 
 ## Set the working directory (Note the direction of the slash)
-
 setwd("c:/projects/learnr/day1/lesson1")
 
 ## Read in the data. Here (Note that we give the dataset a name)
-
 my_data <- read.csv("prewmort.csv")
 
 ## If you want to look at your data: (Note that we supply the name of
 ## the dataset that want to view)
-
 View(my_data)
 
 ## tabulate the year variable. Note that you just need to specify
 ## function that you are using (table) and the name of the dataset (my_data) and
 ## the variable (year) after it.
-
 table(my_data$year)
 
 ## Arguments to a function are comma separated, Some may be optional.
-
 summary(my_data$lsize, digits = 4)
 
 ## If you want to add a new variable to the dataset
-
 my_data$percent_mort <- 100*(my_data$dead/my_data$lsize)
 
 ## Maybe rename a variable:
-
 names(my_data)[names(my_data) == "percent_mort"] <- "pm"
 
 ## If you want to plot something
-
 boxplot(my_data$pm ~ my_data$sow_parity)
 
 ## Then you can save the modified dataset in R format
-
 save(my_data, file = "prewmort.Rda")
--- day1/lesson1/lesson1_3.do
+++ day1/lesson1/lesson1_3.do
@@ -4,42 +4,32 @@
 ** In case a dataset is currently in the memory then we start by
 ** clearing out the environment. Stata can only have one dataset in the
 ** memory at a time
-
 clear
 
 ** Set the working directory (Note the direction of the slash)
-
 cd c:\projects\learnr\day1\lesson1
 
 ** Read in the data. Here
-
 insheet using "prewmort.csv", delimiter(",")
 
 ** If you want to look at your data:
-
 br
 
 ** tabulate the year variable. Note that you just need to specify command
 ** that you are using (table) and the name of the variable after it.
-
 table year
 
 ** Optional inputs to a command go after a comma
-
 summarize lsize, detail
 
 ** If you want to add a new variable to the dataset
-
 generate percent_mort = 100*(dead/lsize)
 
 ** Maybe rename a variable:
-
 rename percent_mort pm
 
 ** If you want to plot something
-
 graph box pm, over(sow_parity)
 
 ** Then you can save the modified dataset in a stata format
-
 save prewmort.dta, replace
